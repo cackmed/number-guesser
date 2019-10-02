7d8707857f2cb73e03be940ee6f9bb685f7d6931
Caleb Pendergraft <cackmed@gmail.com>
2019-10-02 12:46:05 -0700

    imported function into app.js, started setting up response to user input,

--- app.js
+++ app.js
@@ -1,31 +1,28 @@
- 
+import compareNumber from './function.js';
 // Initialize DOM elements
 const guessNumber = document.getElementById('guess-number');
-const guessed1 = document.getElementById('guessed');
-const userGuess = document.getElementById('user-guess');
+const submit = document.getElementById('submit');
+const userGuess = document.getElementById('guessed-number');
 guessNumber.textContent = 4;
-// Set inital state
 let guess = 4;
-// Set Event handlers
-guessed1.addEventListener('click', () => {
-    guess = guess - 1;
+const correctNumber = 3;
 
+// Set Event handlers
+submit.addEventListener('click', () => {
+    guess = guess - 1;
+    const result = compareNumber(userGuess, correctNumber); 
+    if (result === 1) {
+        alert('Your Guess is to high, try lower'); 
+    }
+    else if(result === -1) {
+        alert('Your Guess is to low, try higher'); 
+    }
+    else (result === 0) {
+        alert('Your Guess is Correct, congrats'); 
+    }
     guessNumber.textContent = guess;
     if (guess === 0) {
 
-        document.getElementById('guessed').disabled = true;
+        document.getElementById('submit').disabled = true;
     }
-    
 });
-
-compareNumber(userGuess);
-let guessNum = parseInt(userGuess.value);
-if (userGuess === 3) {
-    alert('Congrats you are correct');
-} 
-else if (userGuess < 3) {
-    alert('Sadly your guess is too high, try lower.');
-}   
-else if (userGuess > 3) {
-    alert('try a higher number');
-} 
